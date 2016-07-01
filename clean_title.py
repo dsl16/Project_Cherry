74d602bb8bbe220caa45571643299cda2868173c
Bing Wang <bing.wang@capitalone.com>
2016-07-01 10:04:51 -0400

    Fixed clean title with reset_index

--- clean_title.py
+++ clean_title.py
@@ -1,4 +1,13 @@
-mojo_df=mojo_df.dropna(subset=["title"])
+#mojo_df=mojo_df.dropna(subset=["title"])
+#mojo_df['cln_title']=pd.DataFrame(map(lambda x: re.sub('[^a-zA-Z0-9]','',x), mojo_df["title"]))
+#meta_df=mojo_df.dropna(subset=["title"])
+#meta_df['cln_title']=pd.DataFrame(map(lambda x: re.sub('[^a-zA-Z0-9]','',x), meta_df["title"]))
+
+
+## Bing:  reset_index fixed the title mis-alignment
+mojo_df = mojo_df.dropna(subset=["title"])
+mojo_df = mojo_df.reset_index(drop=True)
 mojo_df['cln_title']=pd.DataFrame(map(lambda x: re.sub('[^a-zA-Z0-9]','',x), mojo_df["title"]))
-meta_df=mojo_df.dropna(subset=["title"])
+meta_df = meta_df.dropna(subset=["title"])
+meta_df = meta_df.reset_index(drop=True)
 meta_df['cln_title']=pd.DataFrame(map(lambda x: re.sub('[^a-zA-Z0-9]','',x), meta_df["title"]))
